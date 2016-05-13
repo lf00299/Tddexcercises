48e222f76ae2d747a8f7706f4a21172ef3a366f5
Lawrence <lawrence@airportyhs-MacBook-Air.local>
2016-05-13 13:18:35 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,75 @@
+/*
+
+Write your implementation code in here.
+
+*/
+
+function hello(person)
+{
+  if (person)// checks the parameter
+    {
+  return "Hello " + person + "!";
+    }
+
+    else
+    {
+      return "Hello!";
+      }
+
+
+}
+
+
+
+///
+function echo (str)
+  {
+    return str; // will return hello or bye
+  }
+
+
+//
+function shout(str)
+  {
+  return str.toUpperCase();
+  }
+
+
+//
+function repeat(message, times)
+{
+    if (!times)
+    {
+    times = 2;
+    }
+
+    var ret = message
+    for (var i = 1; i < times; i++)
+    {
+      ret = ret + " " + message;
+    }
+      return ret;
+}
+
+
+function start_of_word()
+{
+  return str.toUpperCase;
+}
+
+function repeat () {
+
+
+var ret = '';
+for(var i = 0; i< times; i++)
+{
+
+  if(i === 0)
+  {
+
+  }
+}
+
+
+var it;
+}
